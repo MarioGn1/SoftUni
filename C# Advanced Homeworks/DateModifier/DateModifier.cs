ef7b0f8f983fdcd973afb8beb2503a00776d6346
MarioGn1 <mariogn@mail.bg>
2020-06-18 11:13:07 +0300

    Defining Classes

--- C# Advanced Homeworks/DateModifier/DateModifier.cs
+++ C# Advanced Homeworks/DateModifier/DateModifier.cs
@@ -8,28 +8,36 @@ namespace DateModifier
 {
     class DateModifier
     {
-        private List<DateTime> dates;
+        //        private List<DateTime> dates;
 
-        public List<DateTime> Dates
-        {
-            get => dates;
-            set => dates = value;
-        }
-        
-        public DateModifier()
-        {
-            this.Dates = new List<DateTime>();
-        }
-        public void AddDate(DateTime date)
-        {
-            Dates.Add(date);
-        }
+        //        public List<DateTime> Dates
+        //        {
+        //            get => dates;
+        //            set => dates = value;
+        //        }
 
-        public int GetDifferenceOfTwoDays()
+        //        public DateModifier()
+        //        {
+        //            this.Dates = new List<DateTime>();
+        //        }
+        //        public void AddDate(DateTime date)
+        //        {
+        //            Dates.Add(date);
+        //        }
+
+        //        public int GetDifferenceOfTwoDays()
+        //        {
+        //            TimeSpan difference = Dates[1] - Dates[0];
+
+        //            return int.Parse(difference.Days.ToString());
+        //        }
+
+        public static double GetDaysBetween(string date1, string date2)
         {
-            TimeSpan difference = Dates[1] - Dates[0];
-            
-            return int.Parse(difference.Days.ToString());
+            DateTime one = DateTime.Parse(date1);
+            DateTime two = DateTime.Parse(date2);
+            double days = Math.Abs((one - two).TotalDays);
+            return days;
         }
     }
 }
--- C# Advanced Homeworks/DateModifier/Program.cs
+++ C# Advanced Homeworks/DateModifier/Program.cs
@@ -7,16 +7,21 @@ namespace DateModifier
     {
         static void Main(string[] args)
         {
-            DateModifier dates = new DateModifier();
-            
-            for (int i = 0; i < 2; i++)
-            {
-                int[] date = Console.ReadLine().Split().Select(int.Parse).ToArray();
-                DateTime curDate = new DateTime(date[0],date[1],date[2]);
-                dates.AddDate(curDate);
-            }
+            //DateModifier dates = new DateModifier();
 
-            Console.WriteLine(Math.Abs(dates.GetDifferenceOfTwoDays()));
+            //for (int i = 0; i < 2; i++)
+            //{
+            //    int[] date = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            //    DateTime curDate = new DateTime(date[0],date[1],date[2]);
+            //    dates.AddDate(curDate);
+            //}
+
+            //Console.WriteLine(Math.Abs(dates.GetDifferenceOfTwoDays()));
+
+            string firstDate = Console.ReadLine();
+            string secondDate = Console.ReadLine();
+            double res = DateModifier.GetDaysBetween(firstDate, secondDate);
+            Console.WriteLine(res);
         }
     }
 }
