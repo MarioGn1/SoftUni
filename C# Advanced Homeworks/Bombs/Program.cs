8101ad45d0e1ce07d79a76e2d9e6b354a7eb1a32
MarioGn1 <mariogn@mail.bg>
2020-06-28 11:03:52 +0300

    C# Advanced Exam

---
+++ C# Advanced Homeworks/Bombs/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Bombs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] bombEf = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+            int[] bombCas = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+            
+
+            Queue<int> bombEfects = new Queue<int>();
+            Stack<int> bombCasings = new Stack<int>();
+
+            for (int i = 0; i < bombEf.Length; i++)
+            {
+                bombEfects.Enqueue(bombEf[i]);
+            }
+            for (int i = 0; i < bombCas.Length; i++)
+            {
+                bombCasings.Push(bombCas[i]);
+            }
+
+            int daturaBombs = 0;
+            int cherryBombs = 0;
+            int smokeDecoyBombs = 0;
+
+            bool isFulfill = false;
+
+
+            while (bombEfects.Count > 0 && bombCasings.Count > 0)
+            {
+                int sum = bombEfects.Peek()+bombCasings.Peek();
+
+                if (sum == 40)
+                {
+                    daturaBombs++;
+                    bombEfects.Dequeue();
+                    bombCasings.Pop();
+                }
+                else if (sum == 60)
+                {
+                    cherryBombs++;
+                    bombEfects.Dequeue();
+                    bombCasings.Pop();
+                }
+                else if (sum == 120)
+                {
+                    smokeDecoyBombs++;
+                    bombEfects.Dequeue();
+                    bombCasings.Pop();
+                }
+                else
+                {
+                    bombCasings.Push(bombCasings.Pop() - 5);
+                }
+                if (daturaBombs >= 3 && cherryBombs >=3 && smokeDecoyBombs >=3)
+                {
+                    isFulfill = true;
+                    break;
+                }
+            }
+
+            if (isFulfill)
+            {
+                Console.WriteLine("Bene! You have successfully filled the bomb pouch!");
+            }
+            else
+            {
+                Console.WriteLine("You don't have enough materials to fill the bomb pouch.");
+            }
+            if (bombEfects.Count == 0)
+            {
+                Console.WriteLine("Bomb Effects: empty");
+            }
+            else
+            {
+                Console.WriteLine($"Bomb Effects: {string.Join(", ", bombEfects)}");
+            }
+            if (bombCasings.Count == 0)
+            {
+                Console.WriteLine("Bomb Casings: empty");
+            }
+            else
+            {
+                Console.WriteLine($"Bomb Casings: {string.Join(", ", bombCasings)}");
+            }
+            Console.WriteLine($"Cherry Bombs: {cherryBombs}");
+            Console.WriteLine($"Datura Bombs: {daturaBombs}");
+            Console.WriteLine($"Smoke Decoy Bombs: {smokeDecoyBombs}");
+        }
+    }
+}
---
+++ C# Advanced Homeworks/Parking/Parking/Parking/Parking.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Parking
+{
+    class Parking
+    {
+        private List<Car> data;
+
+        public Parking(string type, int capacity)
+        {
+            this.Type = type;
+            this.Capacity = capacity;
+
+            this.data = new List<Car>();
+        }
+
+        public void Add(Car car)
+        {
+            if (this.Capacity > this.data.Count)
+            {
+                this.data.Add(car);
+            }
+        }
+
+        public bool Remove(string manufacturer, string model)
+        {
+            bool isRemoved = false;
+            if (this.data.Any(el => el.Manufacturer == manufacturer && el.Model == model))
+            {
+                isRemoved = true;
+                this.data = this.data.Where(car => car.Manufacturer != manufacturer && car.Model != model).ToList();
+            }
+            return isRemoved;
+        }
+
+        public Car GetLatestCar()
+        {
+            Car latestCar = this.data.OrderByDescending(el => el.Year).FirstOrDefault();
+            return latestCar;
+        }
+
+        public Car GetCar(string manufacturer, string model)
+        {
+            Car desiredCar = this.data.FirstOrDefault(car => car.Manufacturer == manufacturer && car.Model == model);
+            return desiredCar;
+        }
+
+        public string GetStatistics()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"The cars are parked in {this.Type}:");
+            foreach (Car car in this.data)
+            {
+                sb.AppendLine(car.ToString());
+            }
+            return sb.ToString().Trim();
+        }
+
+        public string Type { get; set; }
+        public int Capacity { get; set; }
+
+        public int Count => this.data.Count;
+    }
+}
