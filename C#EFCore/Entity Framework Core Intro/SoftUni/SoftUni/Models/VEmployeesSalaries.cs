083530d45e7340790afbae57cf33a2e2759c7bc2
Mario <mariogn1@users.noreply.github.com>
2021-03-03 17:11:13 +0200

    EFC Database first patern

---
+++ C#EFCore/Entity Framework Core Intro/SoftUni/SoftUni/Models/VEmployeesSalaries.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SoftUni.Models
+{
+    public partial class VEmployeesSalaries
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public decimal Salary { get; set; }
+    }
+}
