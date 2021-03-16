91963a42be2f80e489a06f939c2f3d9049da0bf1
Mario <mariogn1@users.noreply.github.com>
2021-03-16 09:59:16 +0200

    Auto Mapper

---
+++ C#EFCore/Auto Mapping Objects/FastFood.Core/MappingConfiguration/FastFoodProfile.cs
@@ -0,0 +1,68 @@
+﻿namespace FastFood.Core.MappingConfiguration
+{
+    using AutoMapper;
+    using FastFood.Core.ViewModels.Categories;
+    using FastFood.Core.ViewModels.Employees;
+    using FastFood.Core.ViewModels.Items;
+    using FastFood.Core.ViewModels.Orders;
+    using FastFood.Models;
+    using System;
+    using ViewModels.Positions;
+
+    public class FastFoodProfile : Profile
+    {
+        public FastFoodProfile()
+        {
+            //Positions
+            this.CreateMap<CreatePositionInputModel, Position>()
+                .ForMember(x => x.Name, y => y.MapFrom(s => s.PositionName));
+
+            this.CreateMap<Position, PositionsAllViewModel>()
+                .ForMember(x => x.Name, y => y.MapFrom(s => s.Name));
+            //Employees
+            this.CreateMap<Employee, EmployeesAllViewModel>()
+                .ForMember(x => x.Name, y => y.MapFrom(s => s.Name))
+                .ForMember(x => x.Address, y => y.MapFrom(s => s.Address))
+                .ForMember(x => x.Age, y => y.MapFrom(s => s.Age))
+                .ForMember(x => x.Position, y => y.MapFrom(s => s.Position.Name));
+
+            this.CreateMap<Position, RegisterEmployeeViewModel>()                
+                .ForMember(x => x.PositionId, y => y.MapFrom(s => s.Id));
+
+            this.CreateMap<RegisterEmployeeInputModel, Employee>()
+                .ForMember(x => x.PositionId, y => y.MapFrom(s => s.PositionId))
+                .ForMember(x => x.Name, y => y.MapFrom(s => s.Name))
+                .ForMember(x => x.Address, y => y.MapFrom(s => s.Address))
+                .ForMember(x => x.Age, y => y.MapFrom(s => s.Age));
+            //Categories
+            this.CreateMap<CreateCategoryInputModel, Category>()
+                .ForMember(x => x.Name, y => y.MapFrom(s => s.CategoryName));
+
+            this.CreateMap<Category, CategoryAllViewModel>()
+                .ForMember(x => x.Name, y => y.MapFrom(s => s.Name));
+            //Items
+            this.CreateMap<Category, CreateItemViewModel>()
+                .ForMember(x => x.CategoryId, y => y.MapFrom(s => s.Id));
+
+            this.CreateMap<CreateItemInputModel, Item>()
+                .ForMember(x => x.Name, y => y.MapFrom(s => s.Name))
+                .ForMember(x => x.Price, y => y.MapFrom(s => s.Price))
+                .ForMember(x => x.CategoryId, y => y.MapFrom(s => s.CategoryId));
+
+            this.CreateMap<Item, ItemsAllViewModels>()
+                .ForMember(x => x.Name, y => y.MapFrom(s => s.Name))
+                .ForMember(x => x.Price, y => y.MapFrom(s => s.Price))
+                .ForMember(x => x.Category, y => y.MapFrom(s => s.Category.Name));
+            //Orders
+            this.CreateMap<Order, OrderAllViewModel>()
+                .ForMember(x => x.OrderId, y => y.MapFrom(s => s.Id))
+                .ForMember(x => x.Employee, y => y.MapFrom(s => s.Employee.Name));
+                
+
+            this.CreateMap<CreateOrderInputModel, Order>() 
+                .ForMember(x => x.DateTime, y => y.MapFrom(s => DateTime.UtcNow));
+
+
+        }
+    }
+}
