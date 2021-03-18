4835c0c5866ebb34da52951a421fac67a1066b5d
Mario <mariogn@mail.bg>
2021-03-18 11:18:04 +0200

    JSON Processing - CarDealer - Automapping

---
+++ C#EFCore/JSON Processing/CarDealer/CarDealer/DTO/DtoCar.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace CarDealer.DTO
+{
+    public class DtoCar
+    {
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+
+        public long TravelledDistance { get; set; }
+
+        public List<int> PartsId { get; set; }
+    }
+}
---
+++ C#EFCore/JSON Processing/CarDealer/CarDealer/DTO/DtoSale.cs
@@ -0,0 +1,11 @@
+﻿namespace CarDealer.DTO
+{
+    public class DtoSale
+    {
+        public decimal Discount { get; set; }
+
+        public int CarId { get; set; }
+
+        public int CustomerId { get; set; }
+    }
+}
