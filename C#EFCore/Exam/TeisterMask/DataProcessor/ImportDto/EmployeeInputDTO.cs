c02fd400a52bb6988a83edbdb851d0042b331898
Mario <mariogn@mail.bg>
2021-04-04 16:35:29 +0300

    Exam EF Core

---
+++ C#EFCore/Exam/TeisterMask/DataProcessor/ImportDto/EmployeeInputDTO.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TeisterMask.DataProcessor.ImportDto
+{
+    public class EmployeeInputDTO
+    {
+        [Required]
+        [StringLength(40, MinimumLength = 3)]
+        [RegularExpression("^([A-z0-9]+)$")]
+        public string Username { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [RegularExpression("[0-9]{3}-[0-9]{3}-[0-9]{4}")]
+        public string Phone { get; set; }
+
+        public int[] Tasks { get; set; }
+    }
+}
+
+//•	Username - text with length [3, 40]. Should contain only lower or upper case letters and/or digits. (required)
+//•	Email – text(required).Validate it! There is attribute for this job.
+//•	Phone - text.Consists only of three groups(separated by '-'), the first two consist of three digits and the last one - of 4 digits. (required)
+//•	EmployeesTasks - collection of type EmployeeTask
---
+++ C#EFCore/Exam/TeisterMask/DataProcessor/Serializer.cs
@@ -0,0 +1,77 @@
+﻿namespace TeisterMask.DataProcessor
+{
+    using System;
+    using System.Globalization;
+    using System.Linq;
+    using Data;
+    using Microsoft.EntityFrameworkCore;
+    using Newtonsoft.Json;
+    using ProductShop.Utils;
+    using TeisterMask.Data.Models.Enums;
+    using TeisterMask.DataProcessor.ExportDto;
+    using Formatting = Newtonsoft.Json.Formatting;
+
+    public class Serializer
+    {
+       
+
+        public static string ExportProjectWithTheirTasks(TeisterMaskContext context)
+        {
+            var rootName = "Projects";
+            var dtos = context.Projects
+                .Include(p => p.Tasks)
+                .ToList()
+                .Where(p => p.Tasks.Any())                
+                .Select(p => new ExportProjectsWithTasksDTO
+                {
+                    TasksCount = p.Tasks.Count,
+                    ProjectName = p.Name,
+                    HasEndDate = p.DueDate == null ? "No" : "Yes",
+                    Tasks = p.Tasks.Select(t => new ExportTaskPartialDTO
+                    {
+                        Name = t.Name,
+                        Label = t.LabelType.ToString()
+                    })
+                    .OrderBy(t => t.Name)
+                    .ToList()
+                })
+                .OrderByDescending(p => p.TasksCount)
+                .ThenBy(p => p.ProjectName)                
+                .ToList();
+
+            return XMLCustomSerializer.Serialize(dtos, rootName);
+        }
+
+        public static string ExportMostBusiestEmployees(TeisterMaskContext context, DateTime date)
+        {
+            var employees = context.Employees
+                .Include(e => e.EmployeesTasks)
+                .ThenInclude(et => et.Task)
+                .ToList()
+                .Where(e => e.EmployeesTasks.Any(et => et.Task.OpenDate >= date))
+                .Select(e => new
+                {
+                    Username = e.Username,
+                    Tasks = e.EmployeesTasks
+                    .Where(et => et.Task.OpenDate >= date)
+                    .OrderByDescending(et => et.Task.DueDate)
+                    .ThenBy(et => et.Task.Name)
+                    .Select(et => new
+                    {
+                        TaskName = et.Task.Name,
+                        OpenDate = et.Task.OpenDate.ToString("d", CultureInfo.InvariantCulture),
+                        DueDate = et.Task.DueDate.ToString("d", CultureInfo.InvariantCulture),
+                        LabelType = et.Task.LabelType.ToString(),
+                        ExecutionType = et.Task.ExecutionType.ToString()
+                    })                    
+                    .ToList()
+                })
+                .OrderByDescending(e => e.Tasks.Count)
+                .ThenBy(e => e.Username)
+                .Take(10)
+                .ToList();
+
+            return JsonConvert.SerializeObject(employees, Formatting.Indented);
+        }
+    }
+}
