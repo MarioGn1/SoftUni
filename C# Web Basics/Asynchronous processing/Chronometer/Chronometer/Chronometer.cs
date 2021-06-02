829313ae492a43eff0f2dcb4bd09db25f95380a2
Mario <mariogn@mail.bg>
2021-06-02 17:01:57 +0300

    Async processing

---
+++ C# Web Basics/Asynchronous processing/Chronometer/Chronometer/Chronometer.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Chronometer
+{
+    public class Chronometer : IChronometer
+    {
+        private readonly Stopwatch stopWhatch;
+        private List<string> laps;
+
+        public Chronometer()
+        {
+            this.stopWhatch = new Stopwatch();
+            this.laps = new List<string>();
+        }
+
+        public string GetTime => ElapsedTime(this.stopWhatch);
+
+        public List<string> Laps => this.laps;
+
+        public string Lap()
+        {
+            string formatedLap = ElapsedTime(this.stopWhatch);
+            this.laps.Add(formatedLap);
+            return formatedLap;
+        }
+
+        public void Reset()
+        {
+            stopWhatch.Reset();
+            laps = new List<string>();
+        }
+
+        public void Start()
+        {
+            stopWhatch.Start();
+        }
+
+        public void Stop()
+        {
+            stopWhatch.Stop();
+        }
+
+        private string ElapsedTime(Stopwatch stopwatch)
+        {
+            TimeSpan lap = stopWhatch.Elapsed;
+            string formatedLap = $"{lap.Minutes:D2}:{lap.Seconds:D2}.{lap.Milliseconds:D4}";
+            return formatedLap;
+        }
+    }
+}
---
+++ C# Web Basics/Asynchronous processing/SIS/Demo.App/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using SIS.HTTP.Requests.Contracts;
+using SIS.HTTP.Responses.Contracts;
+using SIS.WebServer.Result;
+
+namespace Demo.App.Controllers
+{
+    public class HomeController : BaseController
+    {
+        public IHttpResponse Home(IHttpRequest httpRequest)
+        {
+            string content = "<h1>Hello, World!</h1>";
+            return new HtmlResult(content, SIS.HTTP.Enums.HttpResponseStatusCode.Ok);
+        }
+    }
+}
